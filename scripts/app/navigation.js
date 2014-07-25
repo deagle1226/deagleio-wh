ecaf068e8331fba61182ec3b42b06b17b3f8e268
Dan Eagle <deagle1226@gmail.com>
2014-07-25 11:52:43 -0700

    bower updates

--- scripts/app/navigation.js
+++ scripts/app/navigation.js
@@ -14,6 +14,7 @@ module.exports = (function() {
         };
         if (options.enter) enter();
         if (options.bindEvents) bindEvents(options.selectors);
+
     };
 
     function goTo(url) {
